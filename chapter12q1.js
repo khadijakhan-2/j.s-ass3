34ca12a50e7e63e5f68251afcfb4c76849519e39
khadijakhan-2 <65037651+khadijakhan-2@users.noreply.github.com>
2020-07-23 19:56:08 +0500

    Add files via upload

---
+++ chapter12q1.js
@@ -0,0 +1,15 @@
+var a= window.prompt("enter any alphabet or number");
+
+if(a >= A && a<=Z )
+{
+    alert("big letter")
+}
+if(a >= a && a<=z )
+{
+    alert("small letter")
+
+}
+if(a >= 1  )
+{
+    alert("NUmbers")
+}
